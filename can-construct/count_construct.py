5f559d34ae6f4902ab855cefc755a88e7582e8fd
Santiago Feijoo <santiago.feijoo96@gmail.com>
2022-04-01 17:53:38 -0300

    count construct memo

---
+++ can-construct/count_construct.py
@@ -0,0 +1,26 @@
+def count_construct(target_word, array, memo={}):
+    if not len(target_word):
+        return 1
+    if target_word in memo:
+        return memo[target_word]
+
+    count = 0
+    for word in array:
+        if target_word.startswith(word):
+            aux = count_construct(target_word[len(word) :], array, memo)
+            count += aux
+    memo[target_word] = count
+    return count
+
+
+print(count_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"], {}))
+print(count_construct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef"], {}))
+print(count_construct("abcdeeeef", ["ab", "abc", "cd", "def", "abcd"], {}))
+print(count_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"], {}))
+print(
+    count_construct(
+        "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+        ["e", "ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee"],
+        {},
+    )
+)
